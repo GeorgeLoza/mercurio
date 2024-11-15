f5ebeb40ba25e0949670f62c948689bd7a3fde60
Abel Diaz <daxes26@gmail.com>
2024-11-15 11:23:17 -0400

    tabla reporte

--- resources/views/livewire/screen-saver.blade.php
+++ resources/views/livewire/screen-saver.blade.php
@@ -11,12 +11,20 @@
 
 
     <div x-data="{ open: @entangle('isVisible') }" x-show="open"
-        class="fixed inset-0 bg-black bg-opacity-90  z-50">
+        class="fixed inset-0 bg-black bg-opacity-90  z-50 flex justify-center items-center">
 <div class=" object-cover">
     {{-- <img src="" alt="Protector de pantalla"
     class="w-[calc(100vw-100px)] h-[calc(100vh-100px)] "> --}}
+
+
+
+
+
+    
     
 <button @click="open = false" class="text-white px-4 py-2  h-32 w-32  rounded"></button> 
+
+
 
 </div>
        </div>
