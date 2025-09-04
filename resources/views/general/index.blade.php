f5d421189e21d9c880cc4a899091d65775d66fe0
Jake78 hp <jorgelozaquispe@gmail.com>
2025-09-04 16:17:05 -0400

    mejora de apariciencia de avisos

--- resources/views/general/index.blade.php
+++ resources/views/general/index.blade.php
@@ -1,212 +1,204 @@
 @extends('layout.app')
 
 @section('titulo')
-Configuración Inicial
-
+    Configuración Inicial
 @endsection
 
 @section('contenido')
-<div class="grid grid-cols-1 md:grid-cols-2 gap-4 p-2">
-    <!--Categoria inicio-->
-    <div class="">
-        <h2 class="text-xl">Categoría</h2>
-        <div class="flex justify-end mb-2">
-            <!--Boton Crear -->
-            <button class="p-2 bg-green-500 rounded-lg"
-                onclick="Livewire.dispatch('openModal', { component: 'general.categoria-producto.crear' })">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 fill-white" viewBox="0 0 448 512">
-                    <path
-                        d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
-                </svg></button>
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-4 p-2">
+        <!--Categoria inicio-->
+        <div class="">
+            <h2 class="text-xl">Categoría</h2>
+            <div class="flex justify-end mb-2">
+                <!--Boton Crear -->
+                <button class="p-2 bg-green-500 rounded-lg"
+                    onclick="Livewire.dispatch('openModal', { component: 'general.categoria-producto.crear' })">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 fill-white" viewBox="0 0 448 512">
+                        <path
+                            d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
+                    </svg></button>
+            </div>
+
+            <!--Tabla -->
+            @livewire('general.categoria-producto.tabla')
+
         </div>
+        <!--categoria fin-->
 
-        <!--Tabla -->
-        @livewire('general.categoria-producto.tabla')
+        <!--Destino inicio-->
+        <div class="">
+            <h2 class="text-xl">Destino</h2>
+            <div class="flex justify-end mb-2">
+                <!--Boton Crear -->
+                <button class="p-2 bg-green-500 rounded-lg"
+                    onclick="Livewire.dispatch('openModal', { component: 'general.destino-producto.crear' })">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 fill-white" viewBox="0 0 448 512">
+                        <path
+                            d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
+                    </svg></button>
+            </div>
 
-    </div>
-    <!--categoria fin-->
+            <!--Tabla -->
+            @livewire('general.destino-producto.tabla')
 
-    <!--Destino inicio-->
-    <div class="">
-        <h2 class="text-xl">Destino</h2>
-        <div class="flex justify-end mb-2">
-            <!--Boton Crear -->
-            <button class="p-2 bg-green-500 rounded-lg"
-                onclick="Livewire.dispatch('openModal', { component: 'general.destino-producto.crear' })">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 fill-white" viewBox="0 0 448 512">
-                    <path
-                        d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
-                </svg></button>
         </div>
+        <!--Destino fin-->
+        <!--Division inicio-->
+        <div class="">
+            <h2 class="text-xl">División</h2>
+            <div class="flex justify-end mb-2">
+                <!--Boton Crear -->
+                <button class="p-2 bg-green-500 rounded-lg"
+                    onclick="Livewire.dispatch('openModal', { component: 'general.division.crear' })">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 fill-white" viewBox="0 0 448 512">
+                        <path
+                            d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
+                    </svg></button>
+            </div>
 
-        <!--Tabla -->
-        @livewire('general.destino-producto.tabla')
+            <!--Tabla -->
+            @livewire('general.division.tabla')
 
-    </div>
-    <!--Destino fin-->
-    <!--Division inicio-->
-    <div class="">
-        <h2 class="text-xl">División</h2>
-        <div class="flex justify-end mb-2">
-            <!--Boton Crear -->
-            <button class="p-2 bg-green-500 rounded-lg"
-                onclick="Livewire.dispatch('openModal', { component: 'general.division.crear' })">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 fill-white" viewBox="0 0 448 512">
-                    <path
-                        d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
-                </svg></button>
         </div>
+        <!--Division fin-->
 
-        <!--Tabla -->
-        @livewire('general.division.tabla')
+        <!--Etapa inicio-->
+        <div class="">
+            <h2 class="text-xl">Etapa</h2>
+            <div class="flex justify-end mb-2">
+                <!--Boton Crear -->
+                <button class="p-2 bg-green-500 rounded-lg"
+                    onclick="Livewire.dispatch('openModal', { component: 'general.etapa.crear' })">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 fill-white" viewBox="0 0 448 512">
+                        <path
+                            d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
+                    </svg></button>
+            </div>
 
-    </div>
-    <!--Division fin-->
+            <!--Tabla -->
+            @livewire('general.etapa.tabla')
 
-    <!--Etapa inicio-->
-    <div class="">
-        <h2 class="text-xl">Etapa</h2>
-        <div class="flex justify-end mb-2">
-            <!--Boton Crear -->
-            <button class="p-2 bg-green-500 rounded-lg"
-                onclick="Livewire.dispatch('openModal', { component: 'general.etapa.crear' })">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 fill-white" viewBox="0 0 448 512">
-                    <path
-                        d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
-                </svg></button>
         </div>
+        <!--Etapa fin-->
 
-        <!--Tabla -->
-        @livewire('general.etapa.tabla')
+        <!--planta inicio-->
+        <div class="">
+            <h2 class="text-xl">Planta</h2>
+            <div class="flex justify-end mb-2">
+                <!--Boton Crear -->
+                <button class="p-2 bg-green-500 rounded-lg"
+                    onclick="Livewire.dispatch('openModal', { component: 'general.planta.crear' })">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 fill-white" viewBox="0 0 448 512">
+                        <path
+                            d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
+                    </svg></button>
+            </div>
 
-    </div>
-    <!--Etapa fin-->
+            <!--Tabla -->
+            @livewire('general.planta.tabla')
 
-    <!--planta inicio-->
-    <div class="">
-        <h2 class="text-xl">Planta</h2>
-        <div class="flex justify-end mb-2">
-            <!--Boton Crear -->
-            <button class="p-2 bg-green-500 rounded-lg"
-                onclick="Livewire.dispatch('openModal', { component: 'general.planta.crear' })">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 fill-white" viewBox="0 0 448 512">
-                    <path
-                        d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
-                </svg></button>
         </div>
+        <!--planta fin-->
 
-        <!--Tabla -->
-        @livewire('general.planta.tabla')
+        <!--unidad inicio-->
+        <div class="">
+            <h2 class="text-xl">Unidad</h2>
+            <div class="flex justify-end mb-2">
+                <!--Boton Crear -->
+                <button class="p-2 bg-green-500 rounded-lg"
+                    onclick="Livewire.dispatch('openModal', { component: 'general.unidad.crear' })">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 fill-white" viewBox="0 0 448 512">
+                        <path
+                            d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
+                    </svg></button>
+            </div>
 
-    </div>
-    <!--planta fin-->
+            <!--Tabla -->
+            @livewire('general.unidad.tabla')
 
-    <!--unidad inicio-->
-    <div class="">
-        <h2 class="text-xl">Unidad</h2>
-        <div class="flex justify-end mb-2">
-            <!--Boton Crear -->
-            <button class="p-2 bg-green-500 rounded-lg"
-                onclick="Livewire.dispatch('openModal', { component: 'general.unidad.crear' })">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 fill-white" viewBox="0 0 448 512">
-                    <path
-                        d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
-                </svg></button>
+
+
+
         </div>
+        <!--unidad fin-->
+        <!--ruta inicio-->
+        <div class="">
+            <h2 class="text-xl">Ruta</h2>
+            <div class="flex justify-end mb-2">
+                <!--Boton Crear -->
+                <button class="p-2 bg-green-500 rounded-lg"
+                    onclick="Livewire.dispatch('openModal', { component: 'general.ruta-acopio.crear' })">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 fill-white" viewBox="0 0 448 512">
+                        <path
+                            d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
+                    </svg></button>
+            </div>
 
-        <!--Tabla -->
-        @livewire('general.unidad.tabla')
+            <!--Tabla -->
+            @livewire('general.ruta-acopio.tabla')
 
-
-
-
-    </div>
-    <!--unidad fin-->
-    <!--ruta inicio-->
-    <div class="">
-        <h2 class="text-xl">Ruta</h2>
-        <div class="flex justify-end mb-2">
-            <!--Boton Crear -->
-            <button class="p-2 bg-green-500 rounded-lg"
-                onclick="Livewire.dispatch('openModal', { component: 'general.ruta-acopio.crear' })">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 fill-white" viewBox="0 0 448 512">
-                    <path
-                        d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
-                </svg></button>
         </div>
+        <!--ruta fin-->
+        <!--subruta inicio-->
+        <div class="">
+            <h2 class="text-xl">Subruta</h2>
+            <div class="flex justify-end mb-2">
+                <!--Boton Crear -->
+                <button class="p-2 bg-green-500 rounded-lg"
+                    onclick="Livewire.dispatch('openModal', { component: 'general.subruta-acopio.crear' })">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 fill-white" viewBox="0 0 448 512">
+                        <path
+                            d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
+                    </svg></button>
+            </div>
 
-        <!--Tabla -->
-        @livewire('general.ruta-acopio.tabla')
+            <!--Tabla -->
+            @livewire('general.subruta-acopio.tabla')
 
-    </div>
-    <!--ruta fin-->
-    <!--subruta inicio-->
-    <div class="">
-        <h2 class="text-xl">Subruta</h2>
-        <div class="flex justify-end mb-2">
-            <!--Boton Crear -->
-            <button class="p-2 bg-green-500 rounded-lg"
-                onclick="Livewire.dispatch('openModal', { component: 'general.subruta-acopio.crear' })">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 fill-white" viewBox="0 0 448 512">
-                    <path
-                        d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
-                </svg></button>
         </div>
+        <!--subruta fin-->
 
-        <!--Tabla -->
-        @livewire('general.subruta-acopio.tabla')
+        <!--subruta inicio-->
+        <div class="">
+            <h2 class="text-xl">Roles</h2>
+            <div class="flex justify-end mb-2">
+                <!--Boton Crear -->
+                <button class="p-2 bg-green-500 rounded-lg"
+                    onclick="Livewire.dispatch('openModal', { component: 'general.roles.crear' })">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 fill-white" viewBox="0 0 448 512">
+                        <path
+                            d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
+                    </svg></button>
+            </div>
 
-    </div>
-    <!--subruta fin-->
+            <!--Tabla -->
+            @livewire('general.roles.tabla')
 
-    <!--subruta inicio-->
-    <div class="">
-        <h2 class="text-xl">Roles</h2>
-        <div class="flex justify-end mb-2">
-            <!--Boton Crear -->
-            <button class="p-2 bg-green-500 rounded-lg"
-                onclick="Livewire.dispatch('openModal', { component: 'general.roles.crear' })">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 fill-white" viewBox="0 0 448 512">
-                    <path
-                        d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
-                </svg></button>
         </div>
+        <!--subruta fin-->
 
-        <!--Tabla -->
-        @livewire('general.roles.tabla')
+        <!--subruta inicio-->
+        <div class="">
+            <h2 class="text-xl">Modulos</h2>
+            <div class="flex justify-end mb-2">
+                <!--Boton Crear -->
+                <button class="p-2 bg-green-500 rounded-lg"
+                    onclick="Livewire.dispatch('openModal', { component: 'general.modulos.crear' })">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 fill-white" viewBox="0 0 448 512">
+                        <path
+                            d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
+                    </svg>
+                </button>
+            </div>
 
-    </div>
-    <!--subruta fin-->
+            <!--Tabla -->
+            @livewire('general.modulos.tabla')
 
-    <!--subruta inicio-->
-    <div class="">
-        <h2 class="text-xl">Modulos</h2>
-        <div class="flex justify-end mb-2">
-            <!--Boton Crear -->
-            <button class="p-2 bg-green-500 rounded-lg"
-                onclick="Livewire.dispatch('openModal', { component: 'general.modulos.crear' })">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 fill-white" viewBox="0 0 448 512">
-                    <path
-                        d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
-                </svg>
-            </button>
         </div>
-
-        <!--Tabla -->
-        @livewire('general.modulos.tabla')
-
+        <!--subruta fin-->
     </div>
-    <!--subruta fin-->
 
     <!--Cambios inicio-->
     @livewire('cambios.index')
-
-
-</div>
-
-
-@livewire('SqlExecutor')
+    @livewire('SqlExecutor')
 @endsection
-
-
-
